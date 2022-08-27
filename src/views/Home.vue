59212e366da59c0c02219f5990ef0ffb4730718b
nehru35 <sonehru@gmail.com>
2022-08-27 10:45:18 +0100

    Lendo os dados e inserindo no formulário

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger: </h1>
+    <Burgerform />
+  </div>
+</template>
+
+<script>
+
+  import Banner from '../components/Banner.vue'
+  import Burgerform from '@/components/Burgerform.vue';
+
+  export default {
+    name: 'Home',
+    components: {
+      Banner,
+      Burgerform
+    }
+  }
+</script>
